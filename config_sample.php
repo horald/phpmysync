347c4cd2f5d82719aee63a98c73b188001f37ee4
horald <horst.meyer.koeln@gmail.com>
2017-07-14 11:54:39 +0200

    undocumented Changes of Version 0.006

---
+++ config_sample.php
@@ -0,0 +1,10 @@
+<?php
+$headline="PhpMySync";
+$database="mysqlitesync.db";
+$dbtyp="SQLITE3";
+$pfad="localhost/own/phpmysync/classes/";
+$autoinc_start= 1;
+$autoinc_step =10;
+$admin="J";
+$debug="N";
+?>
